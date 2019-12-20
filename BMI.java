d9339c57c428a1c8a98de9ab1368636d74a712b2
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2019-12-21 00:19:29 +0100

    Update BMI.java

--- BMI.java
+++ BMI.java
@@ -3,22 +3,32 @@ import java.util.Scanner;
 public class BMI{
     public static void main(String[] args){
         Scanner strana = new Scanner(System.in);
-        Scanner druStrana = new Scanner(System.in);
-        Scanner tretiStrana = new Scanner(System.in);
-        Scanner yes = new Scanner(System.in);
+
 
         
         while (0 < 1){
-            System.out.println("Sup, pls gib délku základny: ");
+            System.out.println("Sup, pls gib délku strany: ");
             int z = strana.nextInt();
 
-            System.out.println("Sup, pls gib délku první odvěsny: ");
-            int y = druStrana.nextInt();
+            System.out.println("Sup, pls gib délku první strany: ");
+            int y = strana.nextInt();
             
-            System.out.println("Sup, pls gib délku druhé odvěsny: ");
-            int x = tretiStrana.nextInt();
+            System.out.println("Sup, pls gib délku druhé strany: ");
+            int x = strana.nextInt();
 
-            if ((x+y) >= z){
+            if (z == 0 || x == 0 || y == 0){
+                System.out.println("Víš, že potřebuješ nejméně tři délky, right ?");
+            }
+
+            else if ((x+y) >= z){
+                System.out.println("Můžeš udělat trojuhelník");
+            }
+
+            else if ((y+z) >=x){
+                System.out.println("Můžeš udělat trojuhelník");
+            }
+
+            else if ((x+z) >=y){
                 System.out.println("Můžeš udělat trojuhelník");
             }
                
